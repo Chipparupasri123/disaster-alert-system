5d6401e2c21ae967e80e3ec5fc4b5b2ae519db0c
Your Name <you@example.com>
2026-04-04 07:37:40 +0530

    fixed map import

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,7 +1,6 @@
-package com.example.demo.controller;
-
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +16,15 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
-    // SIGNUP
-    @PostMapping("/signup")
-    public String signup(@RequestBody User user) {
-        return userService.signup(user);
-    }
-
-    // LOGIN
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User req) {
 
         String result = userService.login(req.getEmail(), req.getPassword());
 
         if (!"Login successful".equals(result)) {
-            return ResponseEntity.status(401).body(result);
+            Map<String, Object> error = new HashMap<>();
+            error.put("message", result);
+            return ResponseEntity.status(401).body(error);
         }
 
         User user = userService.getUserByEmail(req.getEmail());
